1bc1e28b89da0c156062b8f766fe0fab907e407b
kdo_1999/mac <kdo_1999@naver.com>
2024-12-18 17:13:30 +0900

    test: QuestionServiceTest.findAll_question_success

--- src/test/java/com/sbb2/question/service/QuestionServiceTest.java
+++ src/test/java/com/sbb2/question/service/QuestionServiceTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -12,11 +15,16 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.sbb2.infrastructer.question.repository.QuestionRepository;
 import com.sbb2.member.domain.Member;
 import com.sbb2.question.controller.request.QuestionForm;
 import com.sbb2.question.domain.Question;
+import com.sbb2.question.domain.QuestionPageResponse;
 import com.sbb2.question.exception.QuestionBusinessLogicException;
 import com.sbb2.question.exception.QuestionErrorCode;
 
@@ -116,7 +124,7 @@ public class QuestionServiceTest {
 	@DisplayName("질문 수정 성공 테스트")
 	@Test
 	void update_question_succes() {
-	    //given
+		//given
 		QuestionForm questionForm = QuestionForm.builder()
 			.subject("updateSubject")
 			.content("updateContent")
@@ -144,10 +152,11 @@ public class QuestionServiceTest {
 				.content(questionForm.content())
 				.author(givenMember)
 				.build());
-	    //when
-		Question updateQuestion = questionService.update(1L, questionForm.subject(), questionForm.content(), givenMember);
+		//when
+		Question updateQuestion = questionService.update(1L, questionForm.subject(), questionForm.content(),
+			givenMember);
 
-	    //then
+		//then
 		assertThat(updateQuestion.author()).isEqualTo(givenMember);
 		assertThat(updateQuestion.subject()).isEqualTo(questionForm.subject());
 		assertThat(updateQuestion.content()).isEqualTo(questionForm.content());
@@ -156,7 +165,7 @@ public class QuestionServiceTest {
 	@DisplayName("질문 수정시 조회 실패 테스트")
 	@Test
 	void update_find_question_fail() {
-	    //given
+		//given
 		QuestionForm questionForm = QuestionForm.builder()
 			.subject("updateSubject")
 			.content("updateContent")
@@ -173,7 +182,8 @@ public class QuestionServiceTest {
 			.willReturn(Optional.empty());
 
 		//then
-		assertThatThrownBy(() -> questionService.update(1L, questionForm.subject(), questionForm.content(), givenMember))
+		assertThatThrownBy(
+			() -> questionService.update(1L, questionForm.subject(), questionForm.content(), givenMember))
 			.isInstanceOf(QuestionBusinessLogicException.class)
 			.hasMessage(QuestionErrorCode.NOT_FOUND.getMessage());
 	}
@@ -181,7 +191,7 @@ public class QuestionServiceTest {
 	@DisplayName("질문 삭제 성공 테스트")
 	@Test
 	void delete_question_success() {
-	    //given
+		//given
 		Member givenMember = Member.builder()
 			.id(1L)
 			.username("testMember")
@@ -189,7 +199,7 @@ public class QuestionServiceTest {
 			.email("testEmail")
 			.build();
 
-	    given(questionRepository.findById(any(Long.class)))
+		given(questionRepository.findById(any(Long.class)))
 			.willReturn(Optional.of(Question.builder()
 				.id(1L)
 				.subject("subject")
@@ -199,10 +209,79 @@ public class QuestionServiceTest {
 
 		doNothing().when(questionRepository).deleteById(any(Long.class));
 
-	    //when
+		//when
 		questionService.deleteById(1L);
 
 		//then
 		verify(questionRepository, times(1)).deleteById(any(Long.class));
+	}
+
+	@DisplayName("질문 삭제시 질문 조회 실패 테스트")
+	@Test
+	void delete_question_find_fail() {
+		//given
+		Member givenMember = Member.builder()
+			.id(1L)
+			.username("testMember")
+			.password("testPassword")
+			.email("testEmail")
+			.build();
+
+		given(questionRepository.findById(any(Long.class)))
+			.willReturn(Optional.of(Question.builder()
+				.id(1L)
+				.subject("subject")
+				.content("content")
+				.author(givenMember)
+				.build()));
+
+		given(questionRepository.findById(any(Long.class))).willReturn(Optional.empty());
+
+		//then
+		assertThatThrownBy(() -> questionService.deleteById(1L))
+			.isInstanceOf(QuestionBusinessLogicException.class)
+			.hasMessage(QuestionErrorCode.NOT_FOUND.getMessage());
+	}
+
+	@DisplayName("질문 페이징 조회 성공 테스트")
+	@Test
+	void findAll_question_success() {
+		//given
+		List<QuestionPageResponse> questionPageResponseList = new ArrayList<>();
+
+		Member givenMember = Member.builder()
+			.id(1L)
+			.username("testMember")
+			.password("testPassword")
+			.email("testEmail")
+			.build();
+
+		for (int i = 0; i < 15; i++) {
+			questionPageResponseList.add(
+				new QuestionPageResponse(
+					(long)i,
+					"subject" + (i+1),
+					"content" + (i+1),
+					givenMember.username(),
+					LocalDateTime.now(),
+					LocalDateTime.now(),
+					(long)i)
+			);
+		}
+
+		given(questionRepository.findAll(any(String.class), any(Pageable.class))).willReturn(new PageImpl<>(
+			questionPageResponseList,
+			PageRequest.of(0, 10),
+			questionPageResponseList.size()
+			)
+		);
+
+		//when
+		Page<QuestionPageResponse> findAll = questionService.findAll(1, "");
+
+		//then
+		assertThat(findAll.getContent().size()).isEqualTo(10);
+		assertThat(findAll.getTotalElements()).isEqualTo(15);
+		assertThat(findAll.getTotalPages()).isEqualTo(1);
 	}
 }
