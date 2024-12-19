b56611e3ace7fb3bdaa2b71d6ac1ed0e4cb939ae
kdo_1999/mac <kdo_1999@naver.com>
2024-12-19 16:51:56 +0900

    test: VoterServiceTest.delete_answer_voter_success

--- src/test/java/com/sbb2/voter/service/VoterServiceTest.java
+++ src/test/java/com/sbb2/voter/service/VoterServiceTest.java
@@ -164,7 +164,46 @@ public class VoterServiceTest {
 	    //then
 	    assertThat(savedVoter).isEqualTo(voter);
 	}
-	//TODO 댓글 추천 삭제 테스트
+
+	@DisplayName("답변 추천 삭제 성공 테스트")
+	@Test
+	void delete_answer_voter_success() {
+		//given
+		Member member = Member.builder()
+			.id(1L)
+			.username("testUsername")
+			.password("testPassword")
+			.email("testEmail")
+			.build();
+
+		Question question = Question.builder()
+			.id(1L)
+			.subject("testSubject")
+			.content("testContent")
+			.author(member)
+			.build();
+
+		Answer answer = Answer.builder()
+			.id(1L)
+			.content("testContent")
+			.author(member)
+			.build();
+
+		Voter voter = Voter.builder()
+			.id(1L)
+			.answer(answer)
+			.member(member)
+			.build();
+
+		given(voterRepository.findByQuestionIdAndMemberId(answer.id(), member.id())).willReturn(Optional.of(voter));
+		doNothing().when(voterRepository).deleteById(voter.id());
+
+		//when
+		voterService.deleteAnswerVoter(question.id(), member.id());
+
+		//then
+		verify(voterRepository, times(1)).deleteById(voter.id());
+	}
 	//TODO 댓글 중복 추천 방지 테스트
 	//TODO 질문 추천 삭제 테스트
 }
