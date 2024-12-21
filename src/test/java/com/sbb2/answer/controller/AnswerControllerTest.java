50b842f4e465fb59940663c779aac2102d54d66f
kdo_1999/mac <kdo_1999@naver.com>
2024-12-22 00:03:05 +0900

    test: AnswerControllerTest 생성 및 TODO 추가

---
+++ src/test/java/com/sbb2/answer/controller/AnswerControllerTest.java
@@ -0,0 +1,22 @@
+package com.sbb2.answer.controller;
+
+public class AnswerControllerTest {
+
+	//TODO 답변 생성 GET
+
+	//TODO 답변 생성 POST
+
+	//TODO 답변 생성 유효성 검사
+
+	//TODO 답변 수정 GET
+
+	//TODO 답변 수정
+
+	//TODO 답변 수정 유효성 검사
+
+	//TODO 답변 삭제
+
+	//TODO 답변 추천
+
+	//TODO 답변 추천 삭제
+}
