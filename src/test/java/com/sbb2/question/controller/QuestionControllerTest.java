1b955bce910435415dd50cdf07832767a8abdf67
kdo_1999/mac <kdo_1999@naver.com>
2024-12-21 23:07:04 +0900

    test: QuestionControllerTest.findQuestionDetail 추가

--- src/test/java/com/sbb2/question/controller/QuestionControllerTest.java
+++ src/test/java/com/sbb2/question/controller/QuestionControllerTest.java
@@ -1,12 +1,13 @@
 package com.sbb2.question.controller;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,9 +20,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.ui.ConcurrentModel;
 import org.springframework.ui.Model;
 
+import com.sbb2.answer.domain.Answer;
+import com.sbb2.answer.domain.AnswerDetailResponse;
 import com.sbb2.member.domain.Member;
+import com.sbb2.question.domain.Question;
+import com.sbb2.question.domain.QuestionDetailResponse;
 import com.sbb2.question.domain.QuestionPageResponse;
 import com.sbb2.question.service.QuestionService;
+import com.sbb2.voter.domain.Voter;
 
 @ExtendWith(MockitoExtension.class)
 public class QuestionControllerTest {
@@ -55,8 +61,8 @@ public class QuestionControllerTest {
 			questionPageResponseList.add(
 				new QuestionPageResponse(
 					(long)i,
-					"subject" + (i+1),
-					"content" + (i+1),
+					"subject" + (i + 1),
+					"content" + (i + 1),
 					givenMember.username(),
 					LocalDateTime.now(),
 					LocalDateTime.now(),
@@ -75,7 +81,81 @@ public class QuestionControllerTest {
 
 		//then
 		Page<QuestionPageResponse> getAttributePaging = (Page<QuestionPageResponse>)model.getAttribute("paging");
-		Assertions.assertThat(viewName).isEqualTo("question_list");
-		Assertions.assertThat(questionPageResponses).isEqualTo(getAttributePaging);
+		assertThat(viewName).isEqualTo("question_list");
+		assertThat(questionPageResponses).isEqualTo(getAttributePaging);
 	}
+
+	//TODO 질문 단건 조회
+	@DisplayName("질문 단건 조회 성공 테스트")
+	@Test
+	void findQuestionDetail() {
+		//given
+		Member givenMember = Member.builder()
+			.id(1L)
+			.username("testMember")
+			.password("testPassword")
+			.email("testEmail")
+			.build();
+
+		Answer answer = Answer.builder()
+			.id(1L)
+			.content("testContent")
+			.author(givenMember)
+			.build();
+
+		Voter voter = Voter.builder()
+			.member(givenMember)
+			.question(Question.builder().id(1L).build())
+			.build();
+
+		Question question = Question.builder()
+			.id(1L)
+			.subject("subject")
+			.content("content")
+			.author(givenMember)
+			.answerList(List.of(answer))
+			.voterSet(Set.of(voter))
+			.build();
+
+		AnswerDetailResponse answerDetailResponse = AnswerDetailResponse.builder()
+			.id(1L)
+			.questionId(question.id())
+			.voterCount((long)question.answerList().get(0).voterSet().size())
+			.content(question.answerList().get(0).content())
+			.username(givenMember.username())
+			.isVoter(false)
+			.createdAt(LocalDateTime.now())
+			.modifiedAt(LocalDateTime.now())
+			.build();
+
+		QuestionDetailResponse questionDetailResponse = QuestionDetailResponse.builder()
+			.id(question.id())
+			.subject(question.subject())
+			.content(question.content())
+			.voterCount((long)question.voterSet().size())
+			.isVoter(question.voterSet().stream()
+				.anyMatch(v -> v.member().id().equals(givenMember.id())))
+			.answerList(List.of(answerDetailResponse))
+			.createdAt(LocalDateTime.now())
+			.modifiedAt(LocalDateTime.now())
+			.build();
+
+		given(questionService.findDetailById(question.id(), givenMember))
+			.willReturn(questionDetailResponse);
+
+		//when
+		String viewName = questionController.findDetailById(model, question.id(), givenMember);
+
+		//then
+		QuestionDetailResponse getQuestionDetailResponse = (QuestionDetailResponse)model.getAttribute(
+			"questionDetailResponse");
+
+		assertThat(viewName).isEqualTo("question_detail");
+		assertThat(getQuestionDetailResponse).isEqualTo(questionDetailResponse);
+		assertThat(getQuestionDetailResponse.isVoter()).isTrue();
+	}
+
+	//TODO 질문 삭제
+
+	//TODO 질문 추천
 }
