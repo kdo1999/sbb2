1d60b4bd5e14c25eaa32f184def9b894eff7fa6b
kdo_1999/mac <kdo_1999@naver.com>
2024-12-23 14:53:08 +0900

    test: AnswerControllerTest.save_answer_success 추가

--- src/test/java/com/sbb2/answer/controller/AnswerControllerTest.java
+++ src/test/java/com/sbb2/answer/controller/AnswerControllerTest.java
@@ -1,10 +1,95 @@
 package com.sbb2.answer.controller;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import org.hibernate.validator.HibernateValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+
+import com.sbb2.answer.controller.request.AnswerForm;
+import com.sbb2.answer.service.AnswerService;
+import com.sbb2.answer.service.response.AnswerCreateResponse;
+import com.sbb2.common.response.GenericResponse;
+import com.sbb2.member.domain.Member;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+@ExtendWith(MockitoExtension.class)
 public class AnswerControllerTest {
+	@Mock
+	private AnswerService answerService;
+	private AnswerController answerController;
+	private Model model;
+	private Validator validator;
 
-	//TODO 답변 생성 GET
+	@BeforeEach
+	void setUp() {
+		answerController = new AnswerController(answerService);
+		model = new ConcurrentModel();
+		ValidatorFactory factory = Validation.byProvider(HibernateValidator.class)
+			.configure()
+			.buildValidatorFactory();
+		validator = factory.getValidator();
+	}
 
-	//TODO 답변 생성 POST
+	@DisplayName("답변 저장 성공 테스트")
+	@Test
+	void save_answer_success() {
+	    //given
+		AnswerForm answerForm = AnswerForm.builder()
+			.questionId(1L)
+			.content("testContent")
+			.build();
+
+		Member givenMember = Member.builder()
+			.id(1L)
+			.username("testMember")
+			.password("testPassword")
+			.email("testEmail")
+			.build();
+
+		BindingResult bindingResult = new BeanPropertyBindingResult(answerForm, "answerForm");
+		validator.validate(answerForm).forEach(violation ->
+			bindingResult.rejectValue(
+				violation.getPropertyPath().toString(),
+				"error",
+				violation.getMessage()
+			)
+		);
+
+		AnswerCreateResponse answerCreateResponse = AnswerCreateResponse.builder()
+			.questionId(answerForm.questionId())
+			.answerId(1L)
+			.content(answerForm.content())
+			.author(givenMember.username())
+			.build();
+
+		given(answerService.save(answerForm.questionId(), answerForm.content(), givenMember))
+			.willReturn(answerCreateResponse);
+
+		//when
+		ResponseEntity<GenericResponse<AnswerCreateResponse>> result = answerController.save(answerForm,
+			givenMember);
+
+		//then
+		AnswerCreateResponse data = result.getBody().getData();
+		assertThat(answerCreateResponse).isEqualTo(data);
+		assertThat(result.getHeaders().getLocation().getPath()).isEqualTo("/question/" + data.questionId());
+		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+	}
 
 	//TODO 답변 생성 유효성 검사
 
