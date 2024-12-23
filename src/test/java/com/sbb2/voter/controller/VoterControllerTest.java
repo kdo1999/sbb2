6652574940ecbfe14575dc80121cc28376676463
kdo_1999/mac <kdo_1999@naver.com>
2024-12-23 10:30:55 +0900

    test: VoterControllerTest, TODO 추가

---
+++ src/test/java/com/sbb2/voter/controller/VoterControllerTest.java
@@ -0,0 +1,9 @@
+package com.sbb2.voter.controller;
+
+public class VoterControllerTest {
+
+	//TODO 질문 추천 성공 테스트 /voter/quesiton/{id}
+	//TODO 질문 추천 삭제 성공 테스트
+	//TODO 답변 추천 성공 테스트 /voter/answer/{id}
+	//TODO 답변 추천 삭제 성공 테스트
+}
