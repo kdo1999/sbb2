a56ace8c51c0157118aaf8fe1501c704e1a44c57
kdo_1999/mac <kdo_1999@naver.com>
2025-01-06 10:07:18 +0900

    test: AnswerRepositoryTest.find_answerDetailPage_success 추가

--- src/test/java/com/sbb2/infrastructer/answer/repository/AnswerRepositoryTest.java
+++ src/test/java/com/sbb2/infrastructer/answer/repository/AnswerRepositoryTest.java
@@ -14,6 +14,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -21,6 +24,7 @@ import com.sbb2.answer.domain.Answer;
 import com.sbb2.answer.domain.AnswerDetailResponse;
 import com.sbb2.common.config.JpaAudtingConfig;
 import com.sbb2.common.config.QuerydslConfig;
+import com.sbb2.common.util.SearchCondition;
 import com.sbb2.infrastructer.member.repository.MemberRepository;
 import com.sbb2.infrastructer.question.repository.QuestionRepository;
 import com.sbb2.infrastructer.voter.repository.VoterRepository;
@@ -28,11 +32,13 @@ import com.sbb2.member.domain.Member;
 import com.sbb2.question.domain.Question;
 import com.sbb2.voter.domain.Voter;
 
+import lombok.extern.slf4j.Slf4j;
+
 @DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
 @Import({JpaAudtingConfig.class, QuerydslConfig.class})
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DirtiesContext
-public class  AnswerRepositoryTest {
+public class AnswerRepositoryTest {
 	private final AnswerRepository answerRepository;
 	private final QuestionRepository questionRepository;
 	private final MemberRepository memberRepository;
@@ -59,7 +65,14 @@ public class  AnswerRepositoryTest {
 			.email(email)
 			.build();
 
+		Member member2 = Member.builder()
+			.username("testUsername2")
+			.password("testPassword")
+			.email("testEmail2@naver.com")
+			.build();
+
 		Member savedMember = memberRepository.save(member);
+		Member savedMember2 = memberRepository.save(member2);
 
 		String questionSubject = "testSubject";
 		String questionContent = "testContent";
@@ -78,15 +91,38 @@ public class  AnswerRepositoryTest {
 		Question question = questionRepository.findById(1L).get();
 		String answerContent = "answerContent";
 
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 30; i++) {
 			Answer givenAnswer = Answer.builder()
 				.content(answerContent + (i + 1))
 				.author(author)
 				.question(question)
 				.build();
 
+			Voter givenVoter = Voter.builder()
+				.member(author)
+				.answer(givenAnswer)
+				.build();
+
+			givenAnswer.voterSet().add(givenVoter);
+
 			answerRepository.save(givenAnswer);
 		}
+
+		Answer givenAnswer = Answer.builder()
+			.content(answerContent)
+			.author(savedMember)
+			.question(question)
+			.build();
+
+		Voter givenVoter = Voter.builder()
+			.member(savedMember2)
+			.answer(givenAnswer)
+			.build();
+
+		givenAnswer.voterSet().add(givenVoter);
+
+		answerRepository.save(givenAnswer);
+
 	}
 
 	@DisplayName("답변 저장 테스트")
@@ -235,7 +271,7 @@ public class  AnswerRepositoryTest {
 
 		//when
 		AnswerDetailResponse answerDetailResponse = answerRepository.findAnswerDetailByIdAndMemberId(savedAnswer.id(),
-				savedAnswer.author().id()).get();
+			savedAnswer.author().id()).get();
 
 		//then
 		assertThat(answerDetailResponse.id()).isEqualTo(savedAnswer.id());
@@ -247,5 +283,28 @@ public class  AnswerRepositoryTest {
 		assertThat(answerDetailResponse.isVoter()).isEqualTo(savedAnswer.voterSet().stream()
 			.anyMatch(voter -> voter.member().id().equals(member.id())));
 		assertThat(answerDetailResponse.voterCount()).isEqualTo(savedAnswer.voterSet().size());
+	}
+
+	@DisplayName("답변 페이징 조회 성공 테스트")
+	@Test
+	void find_answerDetailPage_success() {
+		//given
+		SearchCondition givenSearchCondition = SearchCondition.builder()
+			.pageNum(0)
+			.build();
+		Long questionId = 1L;
+		Long loginMemberId = 1L;
+
+		Pageable pageable = PageRequest.of(givenSearchCondition.pageNum(), 10);
+
+		//when
+		Page<AnswerDetailResponse> answerDetailResponsePage = answerRepository.findAnswerDetailPageByQuestionId(
+			givenSearchCondition, questionId, loginMemberId, pageable);
+
+		//then
+		List<AnswerDetailResponse> content = answerDetailResponsePage.getContent();
+
+		assertThat(answerDetailResponsePage.getTotalPages()).isEqualTo(4);
+		assertThat(answerDetailResponsePage.getContent().size()).isEqualTo(10);
 	}
 }
