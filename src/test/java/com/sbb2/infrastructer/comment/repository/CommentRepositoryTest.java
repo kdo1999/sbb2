59d3635453c54ec02130594b800650579c1b0063
kdo_1999/mac <kdo_1999@naver.com>
2025-01-06 17:25:14 +0900

    test: CommentRepositoryTest.save_question_comment_success 추가

--- src/test/java/com/sbb2/infrastructer/comment/repository/CommentRepositoryTest.java
+++ src/test/java/com/sbb2/infrastructer/comment/repository/CommentRepositoryTest.java
@@ -1,5 +1,11 @@
 package com.sbb2.infrastructer.comment.repository;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,16 +14,97 @@ import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Repository;
 import org.springframework.test.annotation.DirtiesContext;
 
+import com.sbb2.answer.domain.Answer;
+import com.sbb2.common.config.JpaAudtingConfig;
 import com.sbb2.common.config.QuerydslConfig;
+import com.sbb2.infrastructer.answer.repository.AnswerRepository;
+import com.sbb2.infrastructer.member.repository.MemberRepository;
+import com.sbb2.infrastructer.question.repository.QuestionRepository;
+import com.sbb2.member.domain.Member;
+import com.sbb2.question.domain.Question;
+
+import lombok.extern.slf4j.Slf4j;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
-@Import(QuerydslConfig.class)
+@Import({JpaAudtingConfig.class, QuerydslConfig.class})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DirtiesContext
+@Slf4j
 public class CommentRepositoryTest {
 	private final CommentRepository commentRepository;
+	private final QuestionRepository questionRepository;
+	private final MemberRepository memberRepository;
+	private final AnswerRepository answerRepository;
 
 	@Autowired
-	public CommentRepositoryTest(CommentRepository commentRepository) {
+	public CommentRepositoryTest(CommentRepository commentRepository, QuestionRepository questionRepository,
+		MemberRepository memberRepository, AnswerRepository answerRepository) {
 		this.commentRepository = commentRepository;
+		this.questionRepository = questionRepository;
+		this.memberRepository = memberRepository;
+		this.answerRepository = answerRepository;
+	}
+
+	@BeforeAll
+	void initData() {
+		String username = "testUsername123";
+		String password = "testPassword123";
+		String email = "testEmail123";
+
+		Member member = Member.builder()
+			.username(username)
+			.password(password)
+			.email(email)
+			.build();
+
+		Member savedMember = memberRepository.save(member);
+
+		String questionSubject = "testSubject";
+		String questionContent = "testContent";
+
+		Question question = Question.builder()
+			.subject(questionSubject)
+			.content(questionContent)
+			.author(savedMember)
+			.build();
+
+		Question savedQuestion = questionRepository.save(question);
+
+		String answerContent = "testAnswerContent";
+
+		Answer answer = Answer.builder()
+			.content(answerContent)
+			.question(savedQuestion)
+			.author(savedMember)
+			.build();
+
+		Answer savedAnswer = answerRepository.save(answer);
+	}
+
+	@DisplayName("질문 댓글 저장 성공 테스트")
+	@Test
+	void save_question_comment_success() {
+	    //given
+		Question question = questionRepository.findById(1L).get();
+		Member member = memberRepository.findById(1L).get();
+		String commentContent = "testCommentContent";
+
+		Comment givenComment = Comment.builder()
+			.content(commentContent)
+			.question(question)
+			.author(member)
+			.build();
+
+		//when
+		Comment savedComment = commentRepository.save(givenComment);
+
+	    //then
+		assertThat(savedComment.id()).isNotNull();
+		assertThat(savedComment.content()).isEqualTo(givenComment.content());
+		assertThat(savedComment.question().id()).isEqualTo(givenComment.question().id());
+		assertThat(savedComment.author()).isEqualTo(givenComment.author());
+		assertThat(savedComment.answer()).isNull();
+		assertThat(savedComment.createdAt()).isNotNull();
+		assertThat(savedComment.modifiedAt()).isNotNull();
 	}
 }
