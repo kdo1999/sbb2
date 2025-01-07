9447f4453b345f1f4ed0c737039740c44e7114cf
kdo_1999/mac <kdo_1999@naver.com>
2025-01-07 16:39:29 +0900

    feat: CreateCommentResponse 응답용 객체 추가

---
+++ src/main/java/com/sbb2/comment/service/response/CreateCommentResponse.java
@@ -0,0 +1,21 @@
+package com.sbb2.comment.service.response;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+
+public record CreateCommentResponse(Long commentId, Long parentId, String content, String author,
+									ParentType parentType, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+
+	@Builder
+	public CreateCommentResponse(Long commentId, Long parentId, String content, String author, ParentType parentType,
+		LocalDateTime createdAt, LocalDateTime modifiedAt) {
+		this.commentId = commentId;
+		this.parentId = parentId;
+		this.content = content;
+		this.author = author;
+		this.parentType = parentType;
+		this.createdAt = createdAt;
+		this.modifiedAt = modifiedAt;
+	}
+}
