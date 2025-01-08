6cb4eb16821e1b422c420a747d4cae71bb96142e
kdo_1999/mac <kdo_1999@naver.com>
2025-01-08 10:17:36 +0900

    test: CommentControllerTest setUp 추가

--- src/test/java/com/sbb2/comment/controller/CommentControllerTest.java
+++ src/test/java/com/sbb2/comment/controller/CommentControllerTest.java
@@ -1,5 +1,22 @@
 package com.sbb2.comment.controller;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.sbb2.comment.service.CommentService;
+
 //TODO 01/08 컨트롤러 테스트부터 작성하고 로직 구현할 것
+@ExtendWith(MockitoExtension.class)
 public class CommentControllerTest {
+	@Mock
+	private CommentService commentService;
+
+	private CommentController commentController;
+
+	@BeforeEach
+	void setUp() {
+		commentController = new CommentController(commentService);
+	}
 }
