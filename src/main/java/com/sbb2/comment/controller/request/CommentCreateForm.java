1d7b3f6963959c4540d169247f57857f11a398f7
kdo_1999/mac <kdo_1999@naver.com>
2025-01-08 12:11:04 +0900

    refactor: @Size에 groups 추가

--- src/main/java/com/sbb2/comment/controller/request/CommentCreateForm.java
+++ src/main/java/com/sbb2/comment/controller/request/CommentCreateForm.java
@@ -16,7 +16,7 @@ public record CommentCreateForm(
 	@ValidStringEnum(enumClass = ParentType.class, groups = ValidEnumGroup.class)
 	String parentType,
 	@NotBlank(message = "댓글 내용은 필수 항목입니다.", groups = NotBlankGroup.class)
-	@Size(max = 200, message = "최대 200자까지만 입력 가능합니다.")
+	@Size(max = 200, message = "최대 200자까지만 입력 가능합니다.", groups = SizeGroup.class)
 	String content) {
 
 	@Builder
--- src/main/java/com/sbb2/question/controller/request/QuestionForm.java
+++ src/main/java/com/sbb2/question/controller/request/QuestionForm.java
@@ -8,7 +8,7 @@ import lombok.Builder;
 
 public record QuestionForm(
 	@NotBlank(message = "제목은 필수 항목입니다.", groups = NotBlankGroup.class)
-	@Size(max = 200, message = "최대 200자까지만 입력 가능합니다.")
+	@Size(max = 200, message = "최대 200자까지만 입력 가능합니다.", groups = SizeGroup.class)
 	String subject,
 	@NotBlank(message = "내용은 필수 항목입니다.", groups = NotBlankGroup.class)
 	String content) {
