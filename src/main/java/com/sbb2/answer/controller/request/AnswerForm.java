f8be8fffec5d8b085dacab501b209e9542c48196
kdo_1999/mac <kdo_1999@naver.com>
2025-01-08 11:39:52 +0900

    refactor: AnswerForm 생성자 수정

--- src/main/java/com/sbb2/answer/controller/request/AnswerForm.java
+++ src/main/java/com/sbb2/answer/controller/request/AnswerForm.java
@@ -3,7 +3,6 @@ package com.sbb2.answer.controller.request;
 import static com.sbb2.common.validation.ValidationGroups.*;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
@@ -12,8 +11,7 @@ public record AnswerForm(
 	@NotBlank(message = "답변 내용은 필수 항목입니다.", groups = NotBlankGroup.class) String content) {
 
 	@Builder
-	public AnswerForm(@NotEmpty(message = "질문 ID는 필수 항목입니다.") Long questionId,
-		@NotEmpty(message = "답변 내용은 필수 항목입니다.") String content) {
+	public AnswerForm(Long questionId, String content) {
 		this.questionId = questionId;
 		this.content = content;
 	}
