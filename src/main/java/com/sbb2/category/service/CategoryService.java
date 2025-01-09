af24367c803b461d31b5935363772a66b883ce17
kdo_1999/mac <kdo_1999@naver.com>
2025-01-09 10:10:21 +0900

    feat: 카테고리 ID로 조회 기능 추가

--- src/main/java/com/sbb2/category/service/CategoryService.java
+++ src/main/java/com/sbb2/category/service/CategoryService.java
@@ -1,4 +1,7 @@
 package com.sbb2.category.service;
 
+import com.sbb2.category.domain.Category;
+
 public interface CategoryService {
+	Category findById(Long id);
 }
--- src/main/java/com/sbb2/category/service/CategoryServiceImpl.java
+++ src/main/java/com/sbb2/category/service/CategoryServiceImpl.java
@@ -2,6 +2,7 @@ package com.sbb2.category.service;
 
 import org.springframework.stereotype.Service;
 
+import com.sbb2.category.domain.Category;
 import com.sbb2.infrastructer.category.repository.CategoryRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,4 +11,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 	private final CategoryRepository categoryRepository;
+
+	public Category findById(Long id) {
+		return categoryRepository.findById(id)
+			.orElseThrow(() -> new RuntimeException("해당 질문이 존재하지 않습니다."));
+	}
 }
