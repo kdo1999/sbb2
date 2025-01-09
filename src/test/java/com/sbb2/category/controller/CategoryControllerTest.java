aa914971a3685c1329dd0ee7a0f5ef7065549462
kdo_1999/mac <kdo_1999@naver.com>
2025-01-09 10:45:31 +0900

    test: CategoryControllerTest.findAll_success 추가

--- src/test/java/com/sbb2/category/controller/CategoryControllerTest.java
+++ src/test/java/com/sbb2/category/controller/CategoryControllerTest.java
@@ -1,11 +1,23 @@
 package com.sbb2.category.controller;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import com.sbb2.category.domain.Category;
 import com.sbb2.category.service.CategoryService;
+import com.sbb2.common.response.GenericResponse;
+import com.sbb2.infrastructer.category.entity.CategoryName;
 
 @ExtendWith(MockitoExtension.class)
 public class CategoryControllerTest {
@@ -16,5 +28,38 @@ public class CategoryControllerTest {
 	@BeforeEach
 	void setUp() {
 		categoryController = new CategoryController(categoryService);
+	}
+
+	@DisplayName("카테고리 전체 조회 성공 테스트")
+	@Test
+	void findAll_success() {
+		//given
+		Long givenCategoryId1 = 1L;
+		CategoryName givenCategoryName1 = CategoryName.QUESTION_BOARD;
+
+		Long givenCategoryId2 = 2L;
+		CategoryName givenCategoryName2 = CategoryName.LECTURE_BOARD;
+
+		Category givenCategory1 = Category.builder()
+			.id(givenCategoryId1)
+			.categoryName(givenCategoryName1)
+			.build();
+
+		Category givenCategory2 = Category.builder()
+			.id(givenCategoryId2)
+			.categoryName(givenCategoryName2)
+			.build();
+
+		List<Category> givenCategoryList = List.of(givenCategory1, givenCategory2);
+
+		given(categoryService.findAll()).willReturn(givenCategoryList);
+
+		//when
+		ResponseEntity<GenericResponse<List<Category>>> result = categoryController.findAll();
+
+		//then
+		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(result.getBody().getData()).isEqualTo(givenCategoryList);
+		verify(categoryService, times(1)).findAll();
 	}
 }
