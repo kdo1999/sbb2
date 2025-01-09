035e36ede51044ccbf5f390da64d9972dfda30c8
kdo_1999/mac <kdo_1999@naver.com>
2025-01-09 10:34:37 +0900

    feat: CategoryServiceImpl 트랜잭션 추가

--- src/main/java/com/sbb2/category/service/CategoryServiceImpl.java
+++ src/main/java/com/sbb2/category/service/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.sbb2.category.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sbb2.category.domain.Category;
 import com.sbb2.category.exception.CategoryBusinessLogicException;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CategoryServiceImpl implements CategoryService {
 	private final CategoryRepository categoryRepository;
 
