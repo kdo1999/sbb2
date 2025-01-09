106c777c2f0542e8cc326d441d6a6d560f26ce93
kdo_1999/mac <kdo_1999@naver.com>
2025-01-09 10:45:56 +0900

    feat: 카테고리 전체 조회 기능 추가

--- src/main/java/com/sbb2/category/controller/CategoryController.java
+++ src/main/java/com/sbb2/category/controller/CategoryController.java
@@ -1,9 +1,15 @@
 package com.sbb2.category.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sbb2.category.domain.Category;
 import com.sbb2.category.service.CategoryService;
+import com.sbb2.common.response.GenericResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,5 +19,11 @@ import lombok.RequiredArgsConstructor;
 public class CategoryController {
 	private final CategoryService categoryService;
 
+	@GetMapping
+	public ResponseEntity<GenericResponse<List<Category>>> findAll() {
+		List<Category> categoryList = categoryService.findAll();
 
+		return ResponseEntity.ok()
+			.body(GenericResponse.of(categoryList));
+	}
 }
