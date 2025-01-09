32a928ead530ca2b04242d34e1b6be79020d4d24
kdo_1999/mac <kdo_1999@naver.com>
2025-01-09 10:10:00 +0900

    test: CategoryService.findById 추가

--- src/test/java/com/sbb2/category/service/CategoryServiceTest.java
+++ src/test/java/com/sbb2/category/service/CategoryServiceTest.java
@@ -1,10 +1,19 @@
 package com.sbb2.category.service;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.sbb2.category.domain.Category;
+import com.sbb2.infrastructer.category.entity.CategoryName;
 import com.sbb2.infrastructer.category.repository.CategoryRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,5 +25,27 @@ public class CategoryServiceTest {
 	@BeforeEach
 	void setUp() {
 		categoryService = new CategoryServiceImpl(categoryRepository);
+	}
+
+	@DisplayName("카테고리 ID로 조회 성공 테스트")
+	@Test
+	void save_category_success() {
+	    //given
+		Long givenCategoryId = 1L;
+		CategoryName givenCategoryName = CategoryName.QUESTION_BOARD;
+
+		Category givenCategory = Category.builder()
+			.id(givenCategoryId)
+			.categoryName(givenCategoryName)
+			.build();
+
+		given(categoryRepository.findById(givenCategoryId))
+			.willReturn(Optional.of(givenCategory));
+
+		//when
+		Category findCategory = categoryService.findById(givenCategoryId);
+
+		//then
+		assertThat(findCategory).isEqualTo(givenCategory);
 	}
 }
